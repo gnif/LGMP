3c288e0cb8ea9faeb64279f88885aef31859e767
Geoffrey McRae <geoff@hostfission.com>
2022-01-09 21:24:31 +1100

    [host] fix gcc `parentheses` error

--- lgmp/src/host.c
+++ lgmp/src/host.c
@@ -30,7 +30,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 
 #define LGMP_MAX_QUEUE_TIMEOUT 10000 //10s
 
-#define ALIGN(x) (x + (3)) & ~(3)
+#define ALIGN(x) ((x + (3)) & ~(3))
 
 struct LGMPHostQueue
 {
